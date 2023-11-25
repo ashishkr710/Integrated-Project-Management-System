ffc08ed055a6ba0b14bfa521f1d18752433465b5
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2023-11-26 00:49:19 +0530

    2nd

--- src/main/java/IPMS/Integrated/Project/Management/System/Model/User.java
+++ src/main/java/IPMS/Integrated/Project/Management/System/Model/User.java
@@ -1,4 +1,29 @@
 package IPMS.Integrated.Project.Management.System.Model;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import reactor.util.annotation.NonNull;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+
+    @NotEmpty
+    @Column(nullable = false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty
+    @Email(message = "{error.invalid_email}")
+    private String email;
+
+
 }
