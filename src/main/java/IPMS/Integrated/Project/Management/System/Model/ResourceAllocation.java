a9a5240ef77862a190081e923679954868ee290c
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2024-01-22 23:10:31 +0530

    Controller and Repo Added.

--- src/main/java/IPMS/Integrated/Project/Management/System/Model/ResourceAllocation.java
+++ src/main/java/IPMS/Integrated/Project/Management/System/Model/ResourceAllocation.java
@@ -1,5 +1,8 @@
 package IPMS.Integrated.Project.Management.System.Model;
 
 public class ResourceAllocation {
+    private int resourceId;
+    private String resourceName;
+    private String resourcDescription;
 
 }
