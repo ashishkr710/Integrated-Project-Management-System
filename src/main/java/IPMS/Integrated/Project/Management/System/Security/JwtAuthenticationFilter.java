4677ef36aac71b6e8d9ee50b7690ed0cd6caa99c
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2024-05-01 01:07:33 +0530

    Working On Security

--- src/main/java/IPMS/Integrated/Project/Management/System/Security/JwtAuthenticationFilter.java
+++ src/main/java/IPMS/Integrated/Project/Management/System/Security/JwtAuthenticationFilter.java
@@ -1,42 +1,42 @@
-package IPMS.Integrated.Project.Management.System.Security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String header = request.getHeader("Authorization");
-
-        if (header != null && header.startsWith("Bearer ")) {
-            String token = header.substring(7);
-            String username = jwtUtil.getUsernameFromToken(token);
-
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                if (jwtUtil.validateToken(token, username)) {
-                    Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, null);
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
-            }
-        }
-        filterChain.doFilter(request, response);
-    }
-}
-
+//package IPMS.Integrated.Project.Management.System.Security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//public class JwtAuthenticationFilter extends OncePerRequestFilter {
+//
+//    @Autowired
+//    private JwtUtil jwtUtil;
+//
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        String header = request.getHeader("Authorization");
+//
+//        if (header != null && header.startsWith("Bearer ")) {
+//            String token = header.substring(7);
+//            String username = jwtUtil.getUsernameFromToken(token);
+//
+//            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+//                if (jwtUtil.validateToken(token, username)) {
+//                    Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, null);
+//                    SecurityContextHolder.getContext().setAuthentication(authentication);
+//                }
+//            }
+//        }
+//        filterChain.doFilter(request, response);
+//    }
+//}
+//
