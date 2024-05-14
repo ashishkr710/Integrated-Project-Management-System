f84972cf412a5bd46a796461b5213acc62e0266d
ashishkr710 <77523570+ashishkr710@users.noreply.github.com>
2024-05-14 13:40:18 +0530

    Working On Security

--- src/main/java/IPMS/Integrated/Project/Management/System/Configration/SecurityConfiguration.java
+++ src/main/java/IPMS/Integrated/Project/Management/System/Configration/SecurityConfiguration.java
@@ -47,18 +47,18 @@ public class SecurityConfiguration  {
         return new BCryptPasswordEncoder();
     }
 
-//    @Override
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//        authenticationManagerBuilder
-//                .userDetailsService(customUserDetailsService)
-//                .passwordEncoder(passwordEncoder());
-//    }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
 //
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
